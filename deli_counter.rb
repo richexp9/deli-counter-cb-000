027491fec113a187c243490ebb239d2307d8b086
Richard Loera <richexp9@gmx.com>
2018-01-14 21:38:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def take_a_number(line,name)
   line.push(name)
-  puts "Welcome, #{name}. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{line.find_index} in line."
 end
 
 def now_serving
