e3b73411a20f8449398abc762333ea01222ef95f
Richard Loera <richexp9@gmx.com>
2018-01-14 21:45:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,16 +8,23 @@ def line(line)
     line.each_with_index { |e, i|
        list = "#{list}#{i+1}. #{e} "
     }
+    list = list.slice(0, list.size - 1)
+    puts "The line is currently: #{list}"
   end
-  list
-  binding.pry
+
 end
-line(katz_deli)
+#puts line(katz_deli)
 
 def take_a_number(line,name)
-
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.find_index(name) + 1} in line."
 end
 
-def now_serving
-
+def now_serving(line)
+  if line.size === 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
 end
