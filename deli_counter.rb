c27fc3bbcdd94c57a6a4cb294a4a042175a5b798
Richard Loera <richexp9@gmx.com>
2018-01-14 18:07:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,18 @@
-katz_deli = []
-
+katz_deli = [1,2,3,4]
+require 'pry'
 def line(line)
-
+  if line.size === 0
+    puts "The line is currently empty."
+  else
+    list = ""
+    line.each_with_index { |e, i|
+       list = "#{list}#{i+1}. #{e} "
+    }
+  end
+  list
+  binding.pry
 end
+line(katz_deli)
 
 def take_a_number(line,name)
 
